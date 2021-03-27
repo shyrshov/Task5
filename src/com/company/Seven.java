fc12710365105cf6923e632f3f58c4da5f3f7a5e
Andrii <a.shyrshov@gmail.com>
2021-03-27 19:12:43 +0200

    7

---
+++ src/com/company/Seven.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Seven {
+    //The difference between package private and protected access is in one point - using protected access
+    //we can use variables, methods and constructors even from other package.
+    //If protected member is in the same java environment - we can use Extending of classes and reach access
+    //to needed member with protected access level
+}

1ff832b87b438978598f399b1fb47e4253a0c1d0
Andrii <a.shyrshov@gmail.com>
2021-03-29 00:38:55 +0300

    Final const

---
+++ src/com/company/Ten.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Ten {
+    public final int FINAL_CONST = 10;
+
+    //constants in java can be declared using modifier final
+    //it can be declared just once and can't be changed (compile error will be displayed)
+}
